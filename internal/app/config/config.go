082b6638c09a4c8eab8011c7d3b8a868699bd83e
ujwegh <nikita.aleksandrov@abfprogramming.com>
2023-10-10 18:30:57 +0300

    iter5: env vars

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -5,14 +5,21 @@ import (
 )
 
 type AppConfig struct {
-	ServerAddr       string
-	ShortenedURLAddr string
+	ServerAddr       string `env:"SERVER_ADDRESS"`
+	ShortenedURLAddr string `env:"BASE_URL"`
 }
 
 func ParseFlags() AppConfig {
+	defaultServerAddress := "localhost:8080"
+	defaultShortenedURLAddress := "http://localhost:8080"
+
 	config := AppConfig{}
-	flag.StringVar(&config.ServerAddr, "a", "localhost:8080", "address and port to run server")
-	flag.StringVar(&config.ShortenedURLAddr, "b", "http://localhost:8080", "address and port for shortened url")
+	if config.ServerAddr == "" {
+		flag.StringVar(&config.ServerAddr, "a", defaultServerAddress, "address and port to run server")
+	}
+	if config.ShortenedURLAddr == "" {
+		flag.StringVar(&config.ShortenedURLAddr, "b", defaultShortenedURLAddress, "address and port for shortened url")
+	}
 	flag.Parse()
 	return config
 }
